ffaa0f293d0ca70343316e41d1952403e122e6d2
JayQuattro <82618621+JayQuattro@users.noreply.github.com>
2021-04-15 19:00:57 -0400

    Update ResetDeferedUpdates.ps1
    Added comments

--- Windows/Powershell/Updates/ResetDeferedUpdates.ps1
+++ Windows/Powershell/Updates/ResetDeferedUpdates.ps1
@@ -1,4 +1,17 @@
+<#
+
+Fixes deferred updates settings in windows 10. 
+Disables deferred feature and quality updates
+Sets deffered updates periods to 0 days.
+
+
+#>
+
+#Disable the defer feature updates setting
 Set-ItemProperty -Path "HKLM:HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\WindowsUpdate" -Name "DeferFeatureUpdates" -Value 0
+#Disable the defer quality updates settings
 Set-ItemProperty -Path "HKLM:HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\WindowsUpdate" -Name "DeferQualityUpdates" -Value 0
-Set-ItemProperty -Path "HKLM:HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\WindowsUpdate" -Name "DeferFeatureUpdatesPeriodInDays" -Value 0 
+#Set defer features updates to 0 days 
+Set-ItemProperty -Path "HKLM:HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\WindowsUpdate" -Name "DeferFeatureUpdatesPeriodInDays" -Value 0
+#Set defer quality updates to 0 days
 Set-ItemProperty -Path "HKLM:HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\WindowsUpdate" -Name "DeferQualityUpdatesPeriodInDays" -Value 0
