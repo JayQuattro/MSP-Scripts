9738d42757facb49b07d14bb7c926a44c033aacd
JayQuattro <82618621+JayQuattro@users.noreply.github.com>
2021-06-01 12:12:05 -0400

    Create internationaledge.ps1

---
+++ Windows/Powershell/IE/internationaledge.ps1
@@ -0,0 +1,11 @@
+#Download Edge GP Template
+
+#install Edge Template to 
+#C:\Windows\PolicyDefinitions\msedge.admx 
+ 
+ 
+
+#Set/create GP settings 
+#Set-ItemProperty -Path HKLM:\HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Edge -Name DisableWindowsUpdateAccess -Value "1 
+
+#Change IE regedit settings
